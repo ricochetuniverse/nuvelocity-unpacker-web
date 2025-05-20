5bfb5211e21a3bd1b9e7e479627c6162664199a3
Ng Yik Phang <ngyikp@gmail.com>
2025-05-20 14:34:18 +0100

    Set base URL on Vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: 'https://ricochetuniverse.github.io/nuvelocity-unpacker-web/',
 	plugins: [react()],
 });
