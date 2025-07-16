cafcc1c94b75a5bc4245905b9073d72b60b1a069
Ng Yik Phang <ngyikp@gmail.com>
2025-07-16 23:57:58 +0100

    Remove unneeded DynamicDependency attribute
    Fixed in upstream https://github.com/frankwilco/NuVelocity/issues/2

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices.JavaScript;
 using System.Runtime.Versioning;
 using System.Text.Json;
@@ -11,13 +10,6 @@ public partial class Unpacker
 {
     [JSExport]
     [SupportedOSPlatform("browser")]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(FrameInfo))]
-    // [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Font))]
-    // [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(FontBitmap))]
-    // [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Frame))]
-    // [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(PaletteHolder))]
-    // [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Sequence))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(SequenceFrameInfoList))]
     internal static string ReadFile(byte[] file)
     {
         ImageExporter exporter = new ImageExporter(
