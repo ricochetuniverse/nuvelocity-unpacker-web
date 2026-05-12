9793692047d7cb8c4a05e556f308e0eed1b29b27
Ng Yik Phang <ngyikp@gmail.com>
2026-05-12 21:11:48 +0100

    Update npm dependencies

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,30 +1,22 @@
 import js from '@eslint/js';
+import {defineConfig} from 'eslint/config';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
+	js.configs.recommended,
+	tseslint.configs.strict,
+	reactHooks.configs.flat.recommended,
+	reactRefresh.configs.vite,
 	{
-		ignores: ['dist', 'public/dotnet/'],
-	},
-	{
-		extends: [js.configs.recommended, ...tseslint.configs.strict],
 		files: ['**/*.{ts,tsx}'],
 		languageOptions: {
-			ecmaVersion: 2020,
 			globals: globals.browser,
 		},
-		plugins: {
-			'react-hooks': reactHooks,
-			'react-refresh': reactRefresh,
-		},
-		rules: {
-			...reactHooks.configs.recommended.rules,
-			'react-refresh/only-export-components': [
-				'warn',
-				{allowConstantExport: true},
-			],
-		},
+	},
+	{
+		ignores: ['dist', 'public/dotnet/'],
 	},
 );
